7774330631269bbf001cb9b81458a884e5a908ec
Denis Efremov <efremov.a.denis@gmail.com>
2017-02-23 11:26:50 +0300

    Update theme-footer.vue

--- rest-theme/src/theme-footer.vue
+++ rest-theme/src/theme-footer.vue
@@ -1,26 +1,27 @@
 <style>
-    .footer {
-        font-size: 14px;
-        color: #999;
-        margin-top: 50px;
-    }
+.footer {
+  font-size: 14px;
+  color: #999;
+  margin-top: 50px;
+}
 </style>
 
 <template>
-    <footer class="footer">
-        <div class="container">
-            Copyright &copy; <a v-link="{ path: base_path }">{{ site_name }}</a>
-        </div>
-    </footer>
+  <footer class="footer">
+    <div class="container">
+      Copyright &copy; <a v-link="{ path: basePath }">{{ siteName }}</a>
+    </div>
+  </footer>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                base_path: wp.base_path,
-                site_name: wp.site_name
-            }
-        }
+export default {
+  data() {
+    return {
+      basePath: wp.base_path,
+      siteName: wp.site_name
     }
+  }
+}
 </script>
+
