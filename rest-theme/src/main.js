7ff4e79535410fe108bea3ec5395a122df492079
Denis Efremov <efremov.a.denis@gmail.com>
2017-02-23 11:24:16 +0300

    Update main.js

--- rest-theme/src/main.js
+++ rest-theme/src/main.js
@@ -1,61 +1,73 @@
+/* global wp */
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1)
+}
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-Vue.use(require('vue-resource'));
-Vue.use(VueRouter);
+Vue.use(require('vue-resource'))
+Vue.use(VueRouter)
 
 Vue.config.debug = true
 
 import Posts from './posts.vue'
 import Post from './post.vue'
-Vue.component('Post', Post)
 import Page from './page.vue'
-Vue.component('Page', Page)
 import Header from './theme-header.vue'
-Vue.component('theme-header', Header)
 import Footer from './theme-footer.vue'
+
+Vue.component('Post', Post)
+Vue.component('Page', Page)
+Vue.component('theme-header', Header)
 Vue.component('theme-footer', Footer)
 
-var App = Vue.extend({
-    template: '<theme-header></theme-header>' +
-              '<div class="container"><router-view></router-view></div>' +
-              '<theme-footer></theme-footer>',
+var
+  App = Vue.extend({
+    template: `
+<div>
+  <theme-header></theme-header>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+  <theme-footer></theme-footer>
+<div>`,
 
     ready() {
-        this.updateTitle('');
+      this.updateTitle('')
     },
 
     methods: {
-        updateTitle(pageTitle) {
-            document.title = (pageTitle ? pageTitle + ' - ' : '') + wp.site_name;
-        }
+      updateTitle(pageTitle) {
+        document.title = (pageTitle ? pageTitle + ' - ' : '') + wp.site_name
+      }
     },
 
     events: {
-        'page-title': function(pageTitle) {
-            this.updateTitle(pageTitle);
-        }
+      'page-title': function (pageTitle) {
+        this.updateTitle(pageTitle)
+      }
     }
-});
-var router = new VueRouter({
+  }),
+
+  router = new VueRouter({
     hashbang: false,
     history: true
-});
+  }),
+
+  key, route
 
 router.on(wp.base_path, {
-    component: Posts
-});
+  component: Posts
+})
 
-for (var key in wp.routes) {
-    var route = wp.routes[key];
-    router.on(wp.base_path + route.slug, {
-        component: Vue.component(capitalize(route.type)),
-        postId: route.id
-    });
+for (key in wp.routes) {
+  route = wp.routes[key]
+  router.on(wp.base_path + route.slug, {
+    component: Vue.component(route.type.capitalize()),
+    postId: route.id
+  })
 }
 
-router.start(App, '#app');
-
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+router.start(App, '#app')
